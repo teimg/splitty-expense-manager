639fb3c4bcaf3ab8375f13334d01ff09df004197
Makar Kuleshov <M.Kuleshov@student.tudelft.nl>
2024-02-23 19:52:01 +0100

    Added getters and setters for the bank account class.

--- commons/src/main/java/commons/BankAccount.java
+++ commons/src/main/java/commons/BankAccount.java
@@ -15,4 +15,20 @@ public class BankAccount {
         this.iban = iban;
         this.bic = bic;
     }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
 }
