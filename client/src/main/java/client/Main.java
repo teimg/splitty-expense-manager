db30ee287647297518fad9c8eaf59ed3136871e5
Teim Giesen <t.giesen@student.tudelft.nl>
2024-02-23 16:30:18 +0100

    Adding boilerPlate Statistics Code

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -20,6 +20,7 @@ import static com.google.inject.Guice.createInjector;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import client.scenes.StatisticsScreenCtrl;
 import com.google.inject.Injector;
 
 import client.scenes.AddQuoteCtrl;
@@ -42,8 +43,11 @@ public class Main extends Application {
 
         var overview = FXML.load(QuoteOverviewCtrl.class, "client", "scenes", "QuoteOverview.fxml");
         var add = FXML.load(AddQuoteCtrl.class, "client", "scenes", "AddQuote.fxml");
+        var stats = FXML.load(StatisticsScreenCtrl.class,
+                "client", "scenes", "StatisticsScreen.fxml");
+
 
         var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
-        mainCtrl.initialize(primaryStage, overview, add);
+        mainCtrl.initialize(primaryStage, overview, add, stats);
     }
 }
