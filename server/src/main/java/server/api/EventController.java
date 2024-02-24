038a60e79c602e4a80c5f84f2860ea875c9e75fb
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-02-24 17:25:37 +0100

    EventRepository added, also added as a field to EventController

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -1,9 +1,22 @@
 package server.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.database.EventRepository;
 
 @RestController
 @RequestMapping("/api/event")
 public class EventController {
+
+    @Autowired
+    private final EventRepository repo;
+
+    /**
+     * constructor for event controller
+     * @param repo event repository
+     */
+    public EventController(EventRepository repo) {
+        this.repo = repo;
+    }
 }
