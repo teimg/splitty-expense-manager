934ef55fb3d6f03a6a39345d5f198bdcf8368ac2
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-02-24 17:45:33 +0100

    added one standard method to EventController just to see

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -1,9 +1,13 @@
 package server.api;
 
+import commons.Event;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server.database.EventRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
@@ -18,5 +22,14 @@ public class EventController {
      */
     public EventController(EventRepository repo) {
         this.repo = repo;
+    }
+
+    /**
+     * standard
+     * @return all events
+     */
+    @GetMapping(path = { "", "/" })
+    public List<Event> getAll() {
+        return repo.findAll();
     }
 }
