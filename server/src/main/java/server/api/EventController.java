1673245711122a6fe29d9a394983ebfb6b870d02
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-02-24 17:21:40 +0100

    EventController added

---
+++ server/src/main/java/server/api/EventController.java
@@ -0,0 +1,9 @@
+package server.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+}
