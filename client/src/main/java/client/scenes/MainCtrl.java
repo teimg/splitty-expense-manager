64ce78d3a657ee2058c854af43410be9f708f908
Teim Giesen <t.giesen@student.tudelft.nl>
2024-02-25 12:58:26 +0100

    Added all methods to show scenes

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -32,6 +32,21 @@ public class MainCtrl {
     private AddQuoteCtrl addCtrl;
     private Scene add;
 
+    private AddEditExpenseCtrl addEditExpenseCtrl;
+    private Scene addEdit;
+
+    private InvitationCtrl invitationCtrl;
+    private Scene invitation;
+
+    private OpenDebtsCtrl openDebtsCtrl;
+    private Scene openDebts;
+
+    private StartScreenCtrl startScreenCtrl;
+    private Scene start;
+
+    private StatisticsScreenCtrl statisticsScreenCtrl;
+    private Scene statistics;
+
     @SuppressWarnings("unchecked")
     public void initialize(Stage primaryStage, HashMap<String, Object> sceneMap) {
         this.primaryStage = primaryStage;
@@ -40,7 +55,7 @@ public class MainCtrl {
                 sceneMap.get("QuoteOverviewCtrl");
         Pair<AddQuoteCtrl, Parent> add = (Pair<AddQuoteCtrl, Parent>)
                 sceneMap.get("AddQuoteCtrl");
-        Pair<InvitationCtrl, Parent> addEdit = (Pair<InvitationCtrl, Parent>)
+        Pair<AddEditExpenseCtrl, Parent> addEdit = (Pair<AddEditExpenseCtrl, Parent>)
                 sceneMap.get("AddEditExpenseCtrl");
         Pair<InvitationCtrl, Parent> invite = (Pair<InvitationCtrl, Parent>)
                 sceneMap.get("InvitationCtrl");
@@ -57,7 +72,22 @@ public class MainCtrl {
         this.addCtrl = add.getKey();
         this.add = new Scene(add.getValue());
 
-        showOverview();
+        this.addEditExpenseCtrl = addEdit.getKey();
+        this.addEdit = new Scene(addEdit.getValue());
+
+        this.invitationCtrl = invite.getKey();
+        this.invitation = new Scene(invite.getValue());
+
+        this.openDebtsCtrl = openDebt.getKey();
+        this.openDebts = new Scene(openDebt.getValue());
+
+        this.startScreenCtrl = start.getKey();
+        this.start = new Scene(start.getValue());
+
+        this.statisticsScreenCtrl = stats.getKey();
+        this.statistics = new Scene(stats.getValue());
+
+        showStartScreen();
         primaryStage.show();
     }
 
@@ -72,4 +102,36 @@ public class MainCtrl {
         primaryStage.setScene(add);
         add.setOnKeyPressed(e -> addCtrl.keyPressed(e));
     }
+
+    /**
+     * TODO: ADD CUSTOM METHODS SUCH AS SEEN ABOVE TO EACH OF THESE.
+     * TODO: ALSO ADDING ANY NEW SCENES
+     */
+
+    public void showAddEditExpense() {
+        primaryStage.setTitle("Add/Edit Expense");
+        primaryStage.setScene(addEdit);
+    }
+
+    public void showInvitation() {
+        primaryStage.setTitle("Invitation");
+        primaryStage.setScene(invitation);
+    }
+
+    public void showOpenDebts() {
+        primaryStage.setTitle("Open Debts");
+        primaryStage.setScene(openDebts);
+    }
+
+    public void showStartScreen() {
+        primaryStage.setTitle("Start Screen");
+        primaryStage.setScene(start);
+    }
+
+    public void showStatistics() {
+        primaryStage.setTitle("Statistics");
+        primaryStage.setScene(statistics);
+    }
+
+
 }
