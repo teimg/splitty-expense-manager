d97abb9bb9e947e92a80723ef02863989ebab53c
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-02-25 13:45:20 +0100

    initial commit for skeleton code for participant and expense controller and repo, integration coming soon!

---
+++ server/src/main/java/server/database/ExpenseRepository.java
@@ -0,0 +1,8 @@
+package server.database;
+
+
+import commons.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
