f5f68398be6154d548cf799c8882077cacdab504
Nick de Boer <n.deboer-2@student.tudelft.nl>
2024-02-25 19:48:12 +0100

    ContactInfo boilerplate added

---
+++ client/src/main/java/client/scenes/ContactInfoCtrl.java
@@ -0,0 +1,38 @@
+package client.scenes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ContactInfoCtrl {
+
+    @FXML
+    private TextField BICField;
+
+    @FXML
+    private TextField IBANField;
+
+    @FXML
+    private Button abortButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    void abortButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addButtonPressed(ActionEvent event) {
+
+    }
+
+}
