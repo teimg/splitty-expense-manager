a237961a2a5abb69988a5d0c015a2014dc1d35c4
Feiyang Liu <fliu4@student.tudelft.nl>
2024-02-28 17:40:40 +0100

    Adding put and delete methods in api

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -36,5 +36,28 @@ public class ExpenseController {
         return expense.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    
+    @PutMapping("/{id}")
+    public ResponseEntity<Expense> updateExpense(@PathVariable long id, @RequestBody Expense expenseDetails) {
+        Optional<Expense> expenseData = repo.findById(id);
+        if (expenseData.isPresent()) {
+            Expense expense = expenseData.get();
+            expense.setEvent(expenseDetails.getEvent());
+            expense.setPurchase(expenseDetails.getPurchase());
+            expense.setAmount(expenseDetails.getAmount());
+            expense.setPayer(expenseDetails.getPayer());
+            expense.setDebtors(expenseDetails.getDebtors());
+            Expense updatedExpense = repo.save(expense);
+            return ResponseEntity.ok(updatedExpense);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteExpense(@PathVariable long id) {
+        return repo.findById(id).map(expense -> {
+            repo.deleteById(id);
+            return ResponseEntity.ok().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
