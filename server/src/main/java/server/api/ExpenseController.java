b76d123566811bd99f4340bd3f35bad9cac49364
Feiyang Liu <f.liu-14@student.tudelft.nl>
2024-02-28 17:36:37 +0100

    Revert "Add imports for expense controller"
    This reverts commit e6090afd2399943f4a20311010075a4014b7472e

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -1,13 +1,9 @@
 package server.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import server.database.ExpenseRepository;
-import commons.Expense;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/expense")
