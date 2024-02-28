e6090afd2399943f4a20311010075a4014b7472e
Feiyang Liu <F.Liu-14@student.tudelft.nl>
2024-02-28 17:10:46 +0100

    Add imports for expense controller

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -1,9 +1,13 @@
 package server.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import server.database.ExpenseRepository;
+import commons.Expense;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/expense")
