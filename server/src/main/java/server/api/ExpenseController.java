c239870abe8b1f7dd42c626ccff440bb52748de3
Feiyang Liu <F.Liu-14@student.tudelft.nl>
2024-02-28 17:11:51 +0100

    Create basic boilerplate methods

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -15,11 +15,20 @@ public class ExpenseController {
     @Autowired
     private final ExpenseRepository repo;
 
-    /**
-     * constructor for expense controller
-     * @param repo expense repository
-     */
+    @Autowired
     public ExpenseController(ExpenseRepository repo) {
         this.repo = repo;
     }
+
+    @PostMapping
+    public Expense createExpense(@RequestBody Expense expense) {
+        return repo.save(expense);
+    }
+
+    @GetMapping
+    public List<Expense> getAllExpenses() {
+        return repo.findAll();
+    }
+
+
 }
