73bd0622bec17aabc8abe25dc07e3d958367ae74
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-03-01 13:24:10 +0100

    delete method addition

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -2,9 +2,8 @@ package server.api;
 
 import commons.Event;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import server.database.EventRepository;
 
 import java.util.List;
@@ -31,5 +30,18 @@ public class EventController {
     @GetMapping(path = { "", "/" })
     public List<Event> getAll() {
         return repo.findAll();
+    }
+
+    /**
+     * Searches for an event by id and deletes if such event exists
+     * @param id the ID of the event to be deleted
+     * @return a {@link ResponseEntity} ok if event is found and deleted and notFound otherwise
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        return repo.findById(id).map(event -> {
+            repo.deleteById(id);
+            return ResponseEntity.ok().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
