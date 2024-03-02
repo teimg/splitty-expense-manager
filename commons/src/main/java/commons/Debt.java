0ee5bb351b34595cdec758ac5604f1e0da03feeb
Teim Giesen <t.giesen@student.tudelft.nl>
2024-03-03 00:01:46 +0100

    Addition of all other trivial methods

--- commons/src/main/java/commons/Debt.java
+++ commons/src/main/java/commons/Debt.java
@@ -1,5 +1,7 @@
 package commons;
 
+import java.util.Objects;
+
 public class Debt {
 
     private Participant creditor;
@@ -19,5 +21,61 @@ public class Debt {
         this.debtor = debtor;
         this.amount = amount;
     }
+    
+    public Participant getCreditor() {
+        return creditor;
+    }
 
+    public void setCreditor(Participant creditor) {
+        this.creditor = creditor;
+    }
+
+    public Participant getDebtor() {
+        return debtor;
+    }
+
+    public void setDebtor(Participant debtor) {
+        this.debtor = debtor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Debt debt = (Debt) o;
+
+        if (Double.compare(amount, debt.amount) != 0) return false;
+        if (!Objects.equals(creditor, debt.creditor)) return false;
+        return Objects.equals(debtor, debt.debtor);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = creditor != null ? creditor.hashCode() : 0;
+        result = 31 * result + (debtor != null ? debtor.hashCode() : 0);
+        temp = Double.doubleToLongBits(amount);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Debt{" +
+                "creditor=" + creditor +
+                ", debtor=" + debtor +
+                ", amount=" + amount +
+                '}';
+    }
 }
