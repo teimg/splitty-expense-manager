7e854d54ad96f60d4d06055aa4ec27baae7cd6cb
Nick de Boer <n.deboer-2@student.tudelft.nl>
2024-03-02 21:14:37 +0100

    make flags visible

--- client/src/main/java/client/scenes/MenuBarCtrl.java
+++ client/src/main/java/client/scenes/MenuBarCtrl.java
@@ -3,11 +3,19 @@ package client.scenes;
 import client.language.LanguageSwitch;
 import com.google.inject.Inject;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class MenuBarCtrl implements LanguageSwitch {
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class MenuBarCtrl implements LanguageSwitch, Initializable {
 
     @FXML
     private Menu languageMenu;
@@ -50,6 +58,36 @@ public class MenuBarCtrl implements LanguageSwitch {
 
     private final MainCtrl mainCtrl;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initLanguageMenu();
+    }
+
+
+    public ImageView createImageView(String url){
+        String prefix = "file:client/src/main/resources/client/icons/flags/";
+        int wsize =  32;
+        int hsize = 16;
+        url = prefix + url;
+        ImageView imageView = new ImageView();
+        Image image = new Image(url, true);
+        imageView.setFitWidth(wsize);
+        imageView.setFitHeight(hsize);
+        imageView.setImage(image);
+
+        return imageView;
+    }
+
+    private void setFlagLanguageMenu(String url){
+        languageMenu.setGraphic(createImageView(url));
+    }
+
+    public void initLanguageMenu(){
+        englishButton.setGraphic(createImageView("uk.png"));
+        frenchButton.setGraphic(createImageView("fr.png"));
+        dutchButton.setGraphic(createImageView("nl.png"));
+    }
+
     @Inject
     public MenuBarCtrl(MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
@@ -91,16 +129,19 @@ public class MenuBarCtrl implements LanguageSwitch {
 
     public void setEnglish(ActionEvent actionEvent) {
         mainCtrl.updateLanguage("english");
+        setFlagLanguageMenu("uk.png");
         setLanguage();
     }
 
     public void setDutch(ActionEvent actionEvent) {
         mainCtrl.updateLanguage("dutch");
+        setFlagLanguageMenu("nl.png");
         setLanguage();
     }
 
     public void setFrench(ActionEvent actionEvent) {
         mainCtrl.updateLanguage("french");
+        setFlagLanguageMenu("fr.png");
         setLanguage();
     }
 
@@ -117,4 +158,5 @@ public class MenuBarCtrl implements LanguageSwitch {
         frenchButton.setText(mainCtrl.getTranslator().getTranslation(
                 "MenuBar.French-Button"));
     }
+
 }
