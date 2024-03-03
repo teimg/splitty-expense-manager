eae1ab7fa2bc42823f603507fcaf6b1ef78d3fc8
Feiyang Liu <fliu4@student.tudelft.nl>
2024-03-03 15:11:23 +0100

    Improving and running the basic setup of expense controller test

--- server/src/test/java/server/api/ExpenseControllerTest.java
+++ server/src/test/java/server/api/ExpenseControllerTest.java
@@ -1,22 +1,23 @@
 package server.api;
 
-import commons.Expense;
+import commons.*;
+
+import java.time.LocalDate;
+import java.util.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import server.database.ExpenseRepository;
 import server.service.ExpenseService;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ExpenseControllerTest {
@@ -28,12 +29,22 @@ public class ExpenseControllerTest {
     private ExpenseController expenseController;
 
     private Expense expense;
+    private Event event;
+    private List<Participant> participants;
+    private BankAccount mockAccount;
 
     @BeforeEach
-    public void setup() {
-        expense = new Expense(1, null, "Lunch", 20.0, null, new ArrayList<>(), LocalDate.now());
-    }
+    void setUp() {
 
+        mockAccount = new BankAccount("NL62INGB088899966","6589743");
+        participants = new ArrayList<>();
+        participants.add(new Participant("David",mockAccount));
+        participants.add(new Participant("Jason"));
+        participants.add(new Participant("Shakira"));
+        event = new Event(101, "Jackson", "lfy2024",
+                participants, new Date(2023,05,17),new Date(2023,05,26));
+        expense = new Expense(10086, event, "Lunch", 60.0, new Participant("Jackson"), participants, LocalDate.now());
+    }
     @Test
     public void addExpenseTest() {
         when(expenseService.saveExpense(expense)).thenReturn(expense);
