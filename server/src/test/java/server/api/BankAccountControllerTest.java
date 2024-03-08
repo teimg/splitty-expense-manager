de5577615ead89881469cd2a7f4c225651fd8004
Teim Giesen <t.giesen@student.tudelft.nl>
2024-03-08 12:11:06 +0100

    Boiler plate test code with method signatures

--- server/src/test/java/server/api/BankAccountControllerTest.java
+++ server/src/test/java/server/api/BankAccountControllerTest.java
@@ -1,4 +1,55 @@
 package server.api;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import server.service.BankAccountService;
+
+@ExtendWith(MockitoExtension.class)
 public class BankAccountControllerTest {
+
+    @Mock
+    private BankAccountService service;
+
+    @InjectMocks
+    BankAccountController controller;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void getAllTest() {
+
+    }
+
+    @Test
+    public void getByIdTest() {
+
+    }
+
+    @Test
+    public void addTest() {
+
+    }
+
+    @Test
+    public void deleteTest() {
+
+    }
+
+    @Test
+    public void updateTest() {
+
+    }
+
+    @Test
+    public void nullOrEmptyTest() {
+
+    }
+
 }
