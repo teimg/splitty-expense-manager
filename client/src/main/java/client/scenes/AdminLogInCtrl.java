540edafa81b87585a5f5dc69b706f2031976fad9
Teim Giesen <t.giesen@student.tudelft.nl>
2024-03-19 21:13:07 +0100

    Skeleton addition of FXML file and Java Controller

---
+++ client/src/main/java/client/scenes/AdminLogInCtrl.java
@@ -0,0 +1,25 @@
+package client.scenes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AdminLogInCtrl {
+
+    @FXML
+    public Label logInLabel;
+
+    @FXML
+    public Label passwordLabel;
+
+    @FXML
+    public TextField passwordField;
+
+    @FXML
+    public Button enterButton;
+
+    public void enterPassword(ActionEvent actionEvent) {
+    }
+}
