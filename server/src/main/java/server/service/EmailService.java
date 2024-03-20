829fd800c7ec434453028007c2ec80822a96834b
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-03-20 16:13:08 +0100

    my ide is not recognizing the org.springframework.mail.* and consequensely anything JavaMailSender related. i am questioning wether this is because something is wrong with the imports/dependencies or with my spring. i added methods blindly just to see

---
+++ server/src/main/java/server/service/EmailService.java
@@ -0,0 +1,25 @@
+package server.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        javaMailSender.send(message);
+    }
+
+}
