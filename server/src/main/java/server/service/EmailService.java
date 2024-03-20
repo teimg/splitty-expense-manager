efc92552d56a5815d71b7991ef9d9831f89f5e4b
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-03-20 18:56:28 +0100

    email changed from personal to general

--- server/src/main/java/server/service/EmailService.java
+++ server/src/main/java/server/service/EmailService.java
@@ -2,10 +2,10 @@ package server.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
