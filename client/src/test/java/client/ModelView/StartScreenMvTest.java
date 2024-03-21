8b0fb658acd2306580dbfc937fc4ed161c641cbd
Nick de Boer <n.deboer-2@student.tudelft.nl>
2024-03-21 21:35:08 +0100

    StartscreenMV 100% line coverage with mockito

--- client/src/test/java/client/ModelView/StartScreenMvTest.java
+++ client/src/test/java/client/ModelView/StartScreenMvTest.java
@@ -1,101 +1,51 @@
 package client.ModelView;
 
 
+import client.utils.JoinableEvent;
 import client.utils.RecentEventTracker;
 import client.utils.communicators.interfaces.IEventCommunicator;
 import commons.Event;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.ProcessingException;
+import javafx.beans.property.SimpleListProperty;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.net.ConnectException;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class StartScreenMvTest {
 
     StartScreenMv startScreenMv;
 
-    IEventCommunicator eventCommunicator;
+    @Mock
+    RecentEventTracker mockTracker;
+
+    @Mock
+    IEventCommunicator mockEventCommunicator;
 
     @BeforeEach
     void setup(){
-        eventCommunicator = new IEventCommunicator() {
-            @Override
-            public Event createEvent(Event event) {
-                return event;
-            }
-
-            @Override
-            public Event createEvent(String name) {
-                return new Event(name);
-            }
-
-            @Override
-            public Event getEvent(long id) {
-                return null;
-            }
-
-            @Override
-            public Event getEventByInviteCode(String inviteCode) {
-                if(inviteCode.equals("kTwF02c2")){
-                    return new Event("exists");
-                }
-                throw  new NotFoundException();
-            }
-
-            @Override
-            public Event updateEvent(Event event) {
-                return null;
-            }
-
-            @Override
-            public Event deleteEvent(long id) {
-                return null;
-            }
-        };
-
-        startScreenMv = new StartScreenMv(eventCommunicator, new RecentEventTracker());
-
-
+        startScreenMv = new StartScreenMv(mockEventCommunicator, mockTracker);
     }
 
     @Test
     void testOffline(){
-        eventCommunicator = new IEventCommunicator() {
-            @Override
-            public Event createEvent(Event event) {
-                throw new ProcessingException("");
-            }
 
-            @Override
-            public Event createEvent(String name) {
-                throw new ProcessingException("");
-            }
+        Mockito.when(mockEventCommunicator.createEvent(Mockito.any(Event.class)))
+            .thenThrow(new ProcessingException("Could not reach server"));
 
-            @Override
-            public Event getEvent(long id) {
-                throw new ProcessingException("");
-            }
+        Mockito.when(mockEventCommunicator.getEventByInviteCode(Mockito.anyString()))
+            .thenThrow(new ProcessingException("Could not reach server"));
 
-            @Override
-            public Event getEventByInviteCode(String inviteCode) {
-                throw new ProcessingException("");
-            }
-
-            @Override
-            public Event updateEvent(Event event) {
-                throw new ProcessingException("");
-            }
-
-            @Override
-            public Event deleteEvent(long id) {
-                throw new ProcessingException("");
-            }
-        };
-
-        startScreenMv = new StartScreenMv(eventCommunicator, new RecentEventTracker());
+        Mockito.when(mockEventCommunicator.getEvent(Mockito.anyLong()))
+                .thenThrow(new ProcessingException("Could not reach server"));
 
         assertThrows(ProcessingException.class, () ->{
             startScreenMv.joinEventProperty().setValue("Not empty");
@@ -105,29 +55,46 @@ class StartScreenMvTest {
         assertThrows(ProcessingException.class, () ->{
             startScreenMv.newEventProperty().setValue("Not empty");
             startScreenMv.createEvent();
+
+        }, "ServerOffline");
+
+        assertThrows(ProcessingException.class, () ->{
+            startScreenMv.getRecentEvent(1);
+
         }, "ServerOffline");
     }
 
     @Test
     void createEvent() {
+        Mockito.when(mockEventCommunicator.createEvent(Mockito.any(Event.class)))
+            .thenAnswer( i -> i.getArguments()[0]);
+
         startScreenMv.newEventProperty().setValue("created event");
         assertEquals(new Event("created event"), startScreenMv.createEvent());
 
+
         startScreenMv.newEventProperty().setValue("");
         assertThrows(IllegalArgumentException.class, () ->{
-           startScreenMv.createEvent();
+            startScreenMv.createEvent();
         }, "EventNameEmpty");
     }
 
     @Test
     void joinEvent() {
+        Event ext = new Event("exists");
+        Mockito.when(mockEventCommunicator.getEventByInviteCode("kTwF02c2"))
+            .thenReturn(ext);
+
         startScreenMv.joinEventProperty().setValue("kTwF02c2");
         Event res = startScreenMv.joinEvent();
-        assertEquals(new Event("exists"), res);
+        assertEquals(ext, res);
     }
 
     @Test
     void joinNotExistsEvent() {
+        Mockito.when(mockEventCommunicator.getEventByInviteCode("notValidCode"))
+                .thenThrow(new NotFoundException("404 error??"));
+
         startScreenMv.joinEventProperty().setValue("notValidCode");
         assertThrows(NotFoundException.class, ()->{
             Event res = startScreenMv.joinEvent();
@@ -138,11 +105,49 @@ class StartScreenMvTest {
 
     @Test
     void newEventProperty() {
-        assertEquals(startScreenMv.newEventProperty().getValue(), "");
+        assertEquals("", startScreenMv.newEventProperty().getValue());
     }
 
     @Test
     void joinEventProperty() {
-        assertEquals(startScreenMv.joinEventProperty().getValue(), "");
+        assertEquals("", startScreenMv.joinEventProperty().getValue());
+    }
+
+    @Test
+    void deleteEvent(){
+        assertDoesNotThrow(() ->{
+            startScreenMv.deleteEvent(new JoinableEvent(1, "Henk"));
+        });
+    }
+
+    @Test
+    void init(){
+        assertDoesNotThrow(() ->{
+            startScreenMv.init();
+        });
+    }
+
+    @Test
+    void getRecentEvent(){
+        Event ext = new Event("testEvent");
+        ext.setId(1);
+
+        var mockMeth = Mockito.when(mockEventCommunicator.getEvent(1))
+            .thenThrow(new NotFoundException("blbal bla not found"))
+            .thenReturn(ext);
+
+        assertThrows(NotFoundException.class, ()->{
+           startScreenMv.getRecentEvent(1);
+        }, "CodeNotFound");
+
+        mockMeth.thenReturn(ext);
+
+        assertEquals(ext, startScreenMv.getRecentEvent(1));
+    }
+
+    @Test
+    void recentEventsProperty(){
+        assertEquals(new SimpleListProperty<>(),
+            startScreenMv.recentEventsProperty().get());
     }
 }
