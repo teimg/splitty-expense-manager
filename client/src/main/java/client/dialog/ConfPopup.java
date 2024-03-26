45e49ac942472473ae7b78fea780d987b6a5a613
Nick de Boer <n.deboer-2@student.tudelft.nl>
2024-03-26 01:32:54 +0100

    javadoc added to popup

--- client/src/main/java/client/dialog/ConfPopup.java
+++ client/src/main/java/client/dialog/ConfPopup.java
@@ -10,14 +10,30 @@ public class ConfPopup extends Alert {
         super(AlertType.CONFIRMATION, question , ButtonType.CANCEL, ButtonType.YES);
     }
 
+
+    /**
+     * Create popup without calling new (for efficient calling)
+     * @param question question to ask teh user
+     * @return new ConfPopup
+     */
     public static ConfPopup create(String question){
         return new ConfPopup(question);
     }
 
+    /**
+     * displays the alert and waits for input
+     *
+     * @return the popup
+     */
     public ConfPopup display(){
         this.showAndWait();
         return this;
     }
+
+    /**
+     *
+     * @return true if yes was pressed
+     */
 
     public boolean isConfirmed(){
         return this.getResult() == ButtonType.YES;
