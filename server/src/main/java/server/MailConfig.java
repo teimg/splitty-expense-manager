43851ea4df93c6d4bd77e906b10572fd16a9ed62
Teim Giesen <t.giesen@student.tudelft.nl>
2024-03-27 20:47:51 +0100

    Sending emails functionality

--- server/src/main/java/server/MailConfig.java
+++ server/src/main/java/server/MailConfig.java
@@ -17,7 +17,7 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("ooppteam55@gmail.com");
-        mailSender.setPassword("OOP(P)-team-55");
+        mailSender.setPassword("tczv wzaw dnzj uxmi");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
