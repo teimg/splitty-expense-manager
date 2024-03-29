931feda62f4b4f12217371fd77d291a94b484c3b
Defne Kösecioğlu <d.d.kosecioglu@student.tudelft.nl>
2024-03-29 13:25:27 +0100

    initial refactoring, not that much to refactor in this class since a lot of things are intertwined... but well see

---
+++ client/src/main/java/client/ModelView/StatisticsScreenMv.java
@@ -0,0 +1,55 @@
+package client.ModelView;
+
+import commons.Event;
+import commons.Expense;
+import commons.Tag;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatisticsScreenMv {
+
+    private Event event;
+
+    private double totalPrice;
+
+
+    public StatisticsScreenMv(Event event, double totalPrice) {
+        this.event = event;
+        this.totalPrice = totalPrice;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Map<Tag, Double> fillEntries() {
+        Map<Tag, Double> entries = new HashMap<>();
+        this.totalPrice = 0;
+        for (Expense expense : event.getExpenses()) {
+            totalPrice = totalPrice + expense.getAmount();
+            Tag tag = expense.getTag();
+            if (tag != null) {
+                if (entries.containsKey(tag)) {
+                    double currentSum = entries.get(tag);
+                    entries.put(tag, currentSum + expense.getAmount());
+                } else {
+                    entries.put(tag, expense.getAmount());
+                }
+            }
+        }
+        return entries;
+    }
+}
