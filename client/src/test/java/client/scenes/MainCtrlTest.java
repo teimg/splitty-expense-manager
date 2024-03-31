48d78777e1ef4277f9e47fb9a00b9c153a278092
Jona Bedaux <j.i.bedaux@student.tudelft.nl>
2024-03-31 21:53:33 +0200

    Actually working tests lets gooo

--- client/src/test/java/client/scenes/MainCtrlTest.java
+++ client/src/test/java/client/scenes/MainCtrlTest.java
@@ -19,9 +19,15 @@ import client.language.Translator;
 import client.utils.ClientConfiguration;
 import client.utils.RecentEventTracker;
 import client.utils.scene.SceneController;
+import client.utils.scene.SceneWrapper;
 import com.google.inject.Inject;
 import commons.Event;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import org.junit.jupiter.api.AfterEach;
@@ -31,6 +37,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,6 +47,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class MainCtrlTest {
 
     @Mock
@@ -64,17 +73,34 @@ public class MainCtrlTest {
     @Mock
     private Stage primaryStage;
 
-    @BeforeEach
-    public void setup() {
-        HashMap<String, Object> sceneMap = new HashMap<>(Map.of(
+    private HashMap<String, Object> fakeSceneMap() {
+        return new HashMap<>(Map.of(
                 "StartScreen", new Pair<>(startScreen, fakeParent),
                 "EventOverview", new Pair<>(eventOverview, fakeParent),
                 "MenuBar", new Pair<>(menuBar, fakeParent)
         ));
+    }
+
+    private SceneWrapper mockedSceneWrapper(SceneController sceneController,
+                                            Parent parent) {
+        SceneWrapper mockSceneWrapper = mock(SceneWrapper.class);
+        when(mockSceneWrapper.getSceneController()).thenReturn(sceneController);
+        when(mockSceneWrapper.getParent()).thenReturn(parent);
+
+        return mockSceneWrapper;
+    }
+
+    @BeforeEach
+    public void setup() {
+        HashMap<String, Object> sceneMap = fakeSceneMap();
 
         when(clientConfiguration.getStartupLanguage()).thenReturn("english");
+        when(clientConfiguration.getWindowHeight()).thenReturn(320.);
+        when(clientConfiguration.getWindowWidth()).thenReturn(640.);
 
-        mainCtrl = new MainCtrl(clientConfiguration, translator, recentEventTracker);
+        mainCtrl = new MainCtrl(clientConfiguration, translator, recentEventTracker,
+                this::mockedSceneWrapper,
+                (sceneWrapper, node) -> {});
         mainCtrl.initialize(primaryStage, sceneMap);
     }
 
@@ -82,6 +108,12 @@ public class MainCtrlTest {
     public void setupTest() {
         verify(translator).setCurrentLanguage("english");
         verify(menuBar).setLanguage();
+
+        verify(primaryStage).setHeight(320.);
+        verify(primaryStage).setWidth(640.);
+
+        verify(startScreen).loadInfo();
+        verify(primaryStage).show();
     }
 
     @Test
@@ -110,5 +142,10 @@ public class MainCtrlTest {
         verify(recentEventTracker).registerEvent(evA);
         verify(recentEventTracker).registerEvent(evB);
         verify(recentEventTracker).persistEvents();
+    }
+
+    @Test
+    public void stopTest() {
+
     }
 }
