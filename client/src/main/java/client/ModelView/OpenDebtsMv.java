36799ca13414b852818be4294a1d864302007f04
Teim Giesen <t.giesen@student.tudelft.nl>
2024-03-31 16:10:20 +0200

    Refactoring Open Debts screen to Mv & Ctrl class

--- client/src/main/java/client/ModelView/OpenDebtsMv.java
+++ client/src/main/java/client/ModelView/OpenDebtsMv.java
@@ -1,4 +1,46 @@
 package client.ModelView;
 
+import client.language.Translator;
+import client.scenes.MainCtrl;
+import client.utils.DebtsBuilder;
+import client.utils.communicators.implementations.EmailCommunicator;
+import client.utils.communicators.implementations.ExpenseCommunicator;
+import com.google.inject.Inject;
+import commons.Debt;
+import commons.Event;
+import javafx.scene.control.TitledPane;
+import java.util.ArrayList;
+
 public class OpenDebtsMv {
+
+    private final EmailCommunicator emailCommunicator;
+
+    private final ExpenseCommunicator expenseCommunicator;
+
+    private ArrayList<Debt> debts;
+
+    private ArrayList<TitledPane> panes;
+
+    @Inject
+    public OpenDebtsMv(EmailCommunicator emailCommunicator,
+                       ExpenseCommunicator expenseCommunicator) {
+        this.emailCommunicator = emailCommunicator;
+        this.expenseCommunicator = expenseCommunicator;
+    }
+
+    public void loadInfo(Event event, Translator translator, MainCtrl mainCtrl) {
+        DebtsBuilder debtsBuilder = new DebtsBuilder(event, translator,
+                emailCommunicator, expenseCommunicator, mainCtrl);
+        this.debts = debtsBuilder.getDebts();
+        this.panes = debtsBuilder.getPanes();
+    }
+
+    public boolean checkVisibility() {
+        return debts.isEmpty();
+    }
+
+    public ArrayList<TitledPane> getPanes() {
+        return panes;
+    }
+
 }
