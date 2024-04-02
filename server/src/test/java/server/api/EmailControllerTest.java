3a02b6f6de3463bf7b43f56e69f85ab4ac34bacf
Teim Giesen <t.giesen@student.tudelft.nl>
2024-04-02 19:38:35 +0200

    100% Testing to Event Controller

--- server/src/test/java/server/api/EmailControllerTest.java
+++ server/src/test/java/server/api/EmailControllerTest.java
@@ -1,22 +1,17 @@
 package server.api;
 
 import commons.EmailRequest;
-import commons.PasswordRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
-import server.service.AdminService;
 import server.service.EmailService;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class EmailControllerTest {
--- server/src/test/java/server/api/EventControllerTest.java
+++ server/src/test/java/server/api/EventControllerTest.java
@@ -16,6 +16,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,6 +49,34 @@ public class EventControllerTest {
     }
 
     @Test
+    public void getByIdTest() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e1 = new Event("name", "inviteCode", participants, creationDate, lastActivity);
+        when(service.getById(e1.getId())).thenReturn(Optional.of(e1));
+        ResponseEntity<Event> actual = controller.getByID(e1.getId());
+        assertEquals(e1, actual.getBody());
+        assertEquals(200, actual.getStatusCodeValue());
+    }
+
+    @Test
+    public void getByInviteCodeTest() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e1 = new Event("name", "inviteCode", participants, creationDate, lastActivity);
+        when(service.getByInviteCode("inviteCode")).thenReturn(Optional.of(e1));
+        ResponseEntity<Event> actual = controller.getByInviteCode("inviteCode");
+        assertEquals(e1, actual.getBody());
+        assertEquals(200, actual.getStatusCodeValue());
+    }
+
+    @Test
     public void deleteTest() {
         Participant p1 = new Participant("name1", "email1");
         Participant p2 = new Participant("name2", "email2");
@@ -61,7 +91,7 @@ public class EventControllerTest {
     }
 
     @Test
-    public void updateTest() {
+    public void updateTestSuccessful() {
         Participant p1 = new Participant("name1", "email1");
         Participant p2 = new Participant("name2", "email2");
         List<Participant> participants = List.of(p1, p2);
@@ -77,4 +107,92 @@ public class EventControllerTest {
         assertEquals(e2.getName(), actual.getBody().getName());
         assertEquals(e2.getInviteCode(), actual.getBody().getInviteCode());
     }
+
+    @Test
+    public void updateTestFailure() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e2 = new Event("name2", "inviteCode2", participants, creationDate, lastActivity);
+
+        when(service.getById(1L)).thenReturn(Optional.empty());
+
+        ResponseEntity<Event> actual = controller.update(1L, e2);
+        assertEquals( ResponseEntity.notFound().build(), actual);
+    }
+
+    @Test
+    public void createEventSuccessful() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e1 = new Event("name", "inviteCode", participants, creationDate, lastActivity);
+        when(service.createEvent(e1)).thenReturn(e1);
+        ResponseEntity<Event> actual = controller.createEvent(e1);
+        assertEquals(e1, actual.getBody());
+        assertEquals(200, actual.getStatusCodeValue());
+    }
+
+    @Test
+    public void createEventFailure() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e1 = new Event("name", "inviteCode", participants, creationDate, lastActivity);
+        doThrow(IllegalArgumentException.class).when(service).createEvent(e1);
+        ResponseEntity<Event> actual = controller.createEvent(e1);
+        assertEquals(ResponseEntity.badRequest().build(), actual);
+        assertEquals(400, actual.getStatusCodeValue());
+    }
+
+    @Test
+    public void checkUpdatesSuccess() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e1 = new Event("name", "inviteCode", participants, creationDate, lastActivity);
+        when(service.getById(e1.getId())).thenReturn(Optional.of(e1));
+        ResponseEntity<Event> actual = controller.checkForUpdates(e1.getId());
+        assertEquals(e1, actual.getBody());
+        assertEquals(200, actual.getStatusCodeValue());
+    }
+
+    @Test
+    public void checkUpdatesSuccessTiming() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e1 = new Event("name", "inviteCode", participants, creationDate, lastActivity);
+        e1.setLastActivity(new Date(0L));
+        when(service.getById(e1.getId())).thenReturn(Optional.of(e1));
+        ResponseEntity<Event> actual = controller.checkForUpdates(e1.getId());
+        assertEquals(ResponseEntity.noContent().build(), actual);
+        assertEquals(204, actual.getStatusCodeValue());
+    }
+
+    @Test
+    public void checkUpdatesFailure() {
+        Participant p1 = new Participant("name1", "email1");
+        Participant p2 = new Participant("name2", "email2");
+        List<Participant> participants = List.of(p1, p2);
+        Date creationDate = new Date(2024, 2, 10);
+        Date lastActivity = new Date(2024, 10, 10);
+        Event e1 = new Event("name", "inviteCode", participants, creationDate, lastActivity);
+        e1.setLastActivity(new Date(0L));
+        when(service.getById(e1.getId())).thenReturn(Optional.empty());
+        ResponseEntity<Event> actual = controller.checkForUpdates(e1.getId());
+        assertEquals( ResponseEntity.notFound().build(), actual);
+        assertEquals(404, actual.getStatusCodeValue());
+    }
+
 }
