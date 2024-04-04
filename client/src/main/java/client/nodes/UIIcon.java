f83b84fec0e3a0c4466d61120db1aad0ebf9887f
Nick de Boer <n.deboer-2@student.tudelft.nl>
2024-04-04 17:29:07 +0200

    UI icons added

---
+++ client/src/main/java/client/nodes/UIIcon.java
@@ -0,0 +1,44 @@
+package client.nodes;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class UIIcon extends ImageView {
+    private final int wSize = 14;
+    private final int hSize = 14;
+    private final String path = "file:client/src/main/resources/client/icons/";
+    private final UIIcon.NAME name;
+
+    public UIIcon(UIIcon.NAME name) {
+        this.name = name;
+        this.setImage(createImage());
+    }
+
+    static  public UIIcon icon(UIIcon.NAME name){
+        return new UIIcon(name);
+    }
+
+    private Image createImage(){
+        return new Image(path + name.toString(), wSize, hSize, true, true);
+    }
+
+
+    public enum NAME {
+        DELETE("delete.png"),
+        EDIT("edit.png");
+
+        private final String name;
+
+        NAME(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+    }
+
+
+}
