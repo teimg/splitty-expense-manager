bfdfe1657d5ad5a2d8626b7fd83b22dd077ddb67
Teim Giesen <t.giesen@student.tudelft.nl>
2024-04-04 16:15:34 +0200

    Added try Catch Client Sided to support pop ups

--- client/src/main/java/client/utils/communicators/implementations/CurrencyCommunicator.java
+++ client/src/main/java/client/utils/communicators/implementations/CurrencyCommunicator.java
@@ -3,6 +3,7 @@ package client.utils.communicators.implementations;
 import client.utils.ClientConfiguration;
 import client.utils.communicators.interfaces.ICurrencyCommunicator;
 import com.google.inject.Inject;
+import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.client.ClientBuilder;
 
 import java.time.LocalDate;
@@ -19,16 +20,21 @@ public class CurrencyCommunicator implements ICurrencyCommunicator {
     }
 
     @Override
-    public double getConversion(double amount, String currency, LocalDate date) {
-        return ClientBuilder.newClient()
-                .target(origin)
-                .path("api/currency/{amount}/{currency}/{date}")
-                .resolveTemplate("amount", amount)
-                .resolveTemplate("currency", currency)
-                .resolveTemplate("date", date)
-                .request(APPLICATION_JSON)
-                .accept(APPLICATION_JSON)
-                .get(Double.class);
+    public double getConversion(double amount, String currency, LocalDate date) throws IllegalArgumentException {
+        try {
+            return ClientBuilder.newClient()
+                    .target(origin)
+                    .path("api/currency/{amount}/{currency}/{date}")
+                    .resolveTemplate("amount", amount)
+                    .resolveTemplate("currency", currency)
+                    .resolveTemplate("date", date)
+                    .request(APPLICATION_JSON)
+                    .accept(APPLICATION_JSON)
+                    .get(Double.class);
+        }
+        catch (BadRequestException e) {
+            throw new IllegalArgumentException("Currency Error - Cannot Convert");
+        }
     }
 
 }
