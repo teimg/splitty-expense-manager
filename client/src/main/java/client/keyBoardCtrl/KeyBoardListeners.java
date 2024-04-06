ba03c35ab296007630dd25d0afe633a9da80ce8d
Teim Giesen <t.giesen@student.tudelft.nl>
2024-04-07 00:32:13 +0200

    Added Components

---
+++ client/src/main/java/client/keyBoardCtrl/KeyBoardListeners.java
@@ -0,0 +1,51 @@
+package client.keyBoardCtrl;
+
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class KeyBoardListeners {
+
+    private static final ArrayList<Pair<Scene, ArrayList<EventHandler<KeyEvent>>>> listeners = new ArrayList<>();
+
+    public static void addListener(Scene scene, KeyCode code, KeyPressedAction action) {
+        final boolean[] keyPressed = {false};
+
+        EventHandler<KeyEvent> keyPressedHandler = event -> {
+            if (event.getCode() == code && !keyPressed[0]) {
+                keyPressed[0] = true;
+                action.execute();
+            }
+        };
+
+        EventHandler<KeyEvent> keyReleasedHandler = event -> {
+            if (event.getCode() == code) {
+                keyPressed[0] = false;
+            }
+        };
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyPressedHandler);
+        scene.addEventFilter(KeyEvent.KEY_RELEASED, keyReleasedHandler);
+
+        ArrayList<EventHandler<KeyEvent>> handlers = new ArrayList<>();
+        handlers.add(keyPressedHandler);
+        handlers.add(keyReleasedHandler);
+
+        listeners.add(new Pair<>(scene, handlers));
+    }
+
+    public static void resetListeners() {
+        for (Pair<Scene, ArrayList<EventHandler<KeyEvent>>> pair : listeners) {
+            Scene s = pair.getKey();
+            for (EventHandler<KeyEvent> event : pair.getValue()) {
+                s.removeEventHandler(KeyEvent.KEY_PRESSED, event);
+            }
+        }
+        listeners.clear();
+    }
+
+}
---
+++ client/src/main/java/client/keyBoardCtrl/KeyPressedAction.java
@@ -0,0 +1,7 @@
+package client.keyBoardCtrl;
+
+@FunctionalInterface
+public interface KeyPressedAction {
+    void execute();
+
+}
