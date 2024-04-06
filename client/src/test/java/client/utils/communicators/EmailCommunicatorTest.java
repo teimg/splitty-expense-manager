a2c7722c213c304cc21fdd66008a48f2e4ca1179
Teim Giesen <t.giesen@student.tudelft.nl>
2024-04-06 02:41:30 +0200

    Fixed Remove Participant Bug

--- client/src/test/java/client/utils/communicators/EmailCommunicatorTest.java
+++ client/src/test/java/client/utils/communicators/EmailCommunicatorTest.java
@@ -1,2 +1,82 @@
-package client.utils.communicators;public class EmailCommunicatorTest {
+package client.utils.communicators;
+
+import client.utils.ClientConfiguration;
+import client.utils.communicators.implementations.EmailCommunicator;
+import commons.EmailRequest;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailCommunicatorTest {
+
+    @Mock
+    private ClientConfiguration configuration;
+
+    @Mock
+    private ClientSupplier supplier;
+
+    @Mock
+    private Client client;
+
+    private String origin;
+
+    private EmailCommunicator communicator;
+
+    @BeforeEach
+    public void setUp() {
+        origin = "";
+        MockitoAnnotations.openMocks(this);
+        lenient().when(configuration.getServer()).thenReturn(origin);
+        lenient().when(supplier.getClient()).thenReturn(client);
+        communicator = new EmailCommunicator(configuration, supplier);
+    }
+
+    @Test
+    public void testSend() {
+        WebTarget webTarget = mock(WebTarget.class);
+        Invocation.Builder builder = mock(Invocation.Builder.class);
+        EmailRequest emailRequest = new EmailRequest("To", "Subject", "From");
+        when(client.target(anyString())).thenReturn(webTarget);
+        when(webTarget.path(anyString())).thenReturn(webTarget);
+        when(webTarget.request(APPLICATION_JSON)).thenReturn(builder);
+        when(builder.accept(APPLICATION_JSON)).thenReturn(builder);
+        when(builder.post(any(Entity.class), eq(EmailRequest.class))).thenReturn(emailRequest);
+        assertEquals(emailRequest, communicator.sendEmail(emailRequest));
+        verify(client).target(anyString());
+    }
+
+    @Test
+    public void testGetAll() {
+        WebTarget webTarget = mock(WebTarget.class);
+        Invocation.Builder builder = mock(Invocation.Builder.class);
+        EmailRequest emailRequest = new EmailRequest("To", "Subject", "From");
+        when(client.target(anyString())).thenReturn(webTarget);
+        when(webTarget.path(anyString())).thenReturn(webTarget);
+        when(webTarget.request(APPLICATION_JSON)).thenReturn(builder);
+        when(builder.accept(APPLICATION_JSON)).thenReturn(builder);
+        when(builder.get(EmailRequest.class)).thenReturn(emailRequest);
+        assertEquals(emailRequest, communicator.getAll());
+        verify(client).target(anyString());
+    }
+
+    @Test
+    public void getOrigin() {
+        assertEquals(communicator.getOrigin(), "");
+    }
+
+
+
 }
+
