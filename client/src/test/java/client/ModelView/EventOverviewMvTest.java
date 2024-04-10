b0b0fb22e12b2eaf827ed05ceb0ad835c5b93868
Jona Bedaux <j.i.bedaux@student.tudelft.nl>
2024-04-11 00:33:48 +0200

    Another few bite the dust

--- client/src/test/java/client/ModelView/EventOverviewMvTest.java
+++ client/src/test/java/client/ModelView/EventOverviewMvTest.java
@@ -2,10 +2,15 @@ package client.ModelView;
 
 
 import client.utils.communicators.interfaces.IEventCommunicator;
+import client.utils.communicators.interfaces.IEventUpdateProvider;
 import client.utils.communicators.interfaces.IExpenseCommunicator;
 import client.utils.communicators.interfaces.IParticipantCommunicator;
 import commons.Event;
+import commons.EventChange;
 import commons.Participant;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -36,9 +41,14 @@ public class EventOverviewMvTest {
     @Mock
     private IExpenseCommunicator expenseCommunicator;
 
+    @Mock
+    private IEventUpdateProvider eventUpdateProvider;
+
 
     private Event event;
     private Participant participant;
+
+    private EventOverviewMv mv;
 
 
     @BeforeEach
@@ -46,27 +56,35 @@ public class EventOverviewMvTest {
         MockitoAnnotations.initMocks(this);
         this.event = new Event("name");
         this.participant = new Participant();
+
+        mv = new EventOverviewMv(
+                eventCommunicator,
+                participantCommunicator,
+                expenseCommunicator,
+                eventUpdateProvider
+        );
     }
 
 
     @Test
     void testGetSetEvent() {
-        EventOverviewMv mv = new EventOverviewMv(eventCommunicator, participantCommunicator, expenseCommunicator);
-        mv.setEvent(event);
+        when(eventUpdateProvider.event()).thenReturn(event);
         assertEquals(event, mv.getEvent());
     }
 
 
     @Test
     void getEventCommunicator() {
-        EventOverviewMv mv = new EventOverviewMv(eventCommunicator, participantCommunicator, expenseCommunicator);
         assertNotNull(mv.getEventCommunicator());
     }
 
+    @Test
+    void getEventUpdateProvider() {
+        assertNotNull(mv.getEventUpdateProvider());
+    }
 
     @Test
     void testGetSetSelectedPayer() {
-        EventOverviewMv mv = new EventOverviewMv(eventCommunicator, participantCommunicator, expenseCommunicator);
         mv.setSelectedPayer(participant);
         assertEquals(participant, mv.getSelectedPayer());
     }
@@ -74,14 +92,13 @@ public class EventOverviewMvTest {
 
     @Test
     void testCopyInviteCode() {
-        EventOverviewMv mv = new EventOverviewMv(eventCommunicator, participantCommunicator);
         Clipboard clipboardMock = mock(Clipboard.class);
         when(Clipboard.getSystemClipboard()).thenReturn(clipboardMock);
 
         ClipboardContent contentMock = mock(ClipboardContent.class);
         when(clipboardMock.getContent(DataFormat.PLAIN_TEXT)).thenReturn(contentMock);
 
-        mv.setEvent(event);
+        when(eventUpdateProvider.event()).thenReturn(event);
         when(event.getInviteCode()).thenReturn("invite code");
 
         mv.copyInviteCode();
@@ -93,7 +110,6 @@ public class EventOverviewMvTest {
 
     @Test
     void deleteParticipantTest() {
-        EventOverviewMv mv = new EventOverviewMv(eventCommunicator, participantCommunicator, expenseCommunicator);
         Optional<Participant> optionalParticipant = Optional.of(participant);
         participant.setId(592L);
         mv.deleteParticipant(optionalParticipant);
@@ -105,18 +121,20 @@ public class EventOverviewMvTest {
 
     @Test
     void eventCommunicatorCheckForUpdateTest() {
-        EventOverviewMv mv = new EventOverviewMv(eventCommunicator, participantCommunicator, expenseCommunicator);
+        EventChange change = new EventChange();
+
         long eventId = 811L;
-        when(eventCommunicator.checkForEventUpdates(eventId)).thenReturn(event);
+        when(eventCommunicator.checkForEventUpdates(eventId)).thenReturn(change);
 
 
-        assertEquals(event, mv.eventCommunicatorCheckForUpdate(eventId));
+        assertEquals(change, mv.eventCommunicatorCheckForUpdate(eventId));
     }
 
     @Test
     void eventCommunicatorGetEventTest() {
-        EventOverviewMv mv = new EventOverviewMv(eventCommunicator, participantCommunicator, expenseCommunicator);
-        mv.setEvent(event);
+
+        when(eventUpdateProvider.event()).thenReturn(event);
+
         event.setId(333L);
         when(eventCommunicator.getEvent(333L)).thenReturn(event);
 
